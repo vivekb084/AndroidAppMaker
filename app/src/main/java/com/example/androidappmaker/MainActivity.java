04bf9f5f709e9814596a4e24f92621661970da3c
vivek.bindal <vivek.bindal@ebizontek.com>
2020-08-08 22:38:48 +0530

    new maker created

---
+++ app/src/main/java/com/example/androidappmaker/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.androidappmaker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().hide();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.loadUrl("https://github.com/vivekb084");
+        mWebView.setWebViewClient(new WebViewClient());
+
+    }
+}
